51747406bd9c2504b04007753ca9d34a8878ec9e
Elyura <hhumbetovaa@gmail.com>
2026-07-29 15:04:02 +0400

    Initial Commit

---
+++ Chess/ChessApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChessApp.swift
+//  Chess
+//
+//  Created by Elyura on 29.07.26.
+//
+
+import SwiftUI
+
+@main
+struct ChessApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

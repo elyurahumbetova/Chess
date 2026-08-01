f0fa2a7eee24124589372b758c335822b727b4c5
Elyura <hhumbetovaa@gmail.com>
2026-08-01 13:05:18 +0400

    feat: bishop and knight models created

---
+++ Chess/Models/Bishop.swift
@@ -0,0 +1,52 @@
+//
+//  Bishop.swift
+//  Chess
+//
+//  Created by Elyura on 01.08.26.
+//
+
+import SwiftUI
+
+class Bishop: Piece{
+    let id: UUID
+    let team: Team
+    var hasMoved: Bool
+    
+    init(team: Team) {
+        self.id = UUID()
+        self.team = team
+        self.hasMoved = false
+    }
+    
+    func possibleMovements(from position: Position, board: Board) -> [Position] {
+        var moves: [Position] = []
+        
+        let directions = [
+            (1,-1),
+            (1,1),
+            (-1,1),
+            (-1,-1)
+        ]
+        
+        for direction in directions{
+            var currentPosition = position
+            
+            while let nextPosition = currentPosition.offset(rowBy: direction.0, rankBy: direction.1){
+                if let targetPiece = board[position]{
+                    if targetPiece.team != team{
+                        moves.append(nextPosition)
+                    }
+                    break
+                }else{
+                    moves.append(nextPosition)
+                }
+                
+                currentPosition = nextPosition
+            }
+        }
+        
+        return moves
+    }
+    
+    
+}
---
+++ Chess/Models/Knight.swift
@@ -0,0 +1,55 @@
+//
+//  Knight.swift
+//  Chess
+//
+//  Created by Elyura on 01.08.26.
+//
+
+import SwiftUI
+
+class Knight: Piece{
+    let id: UUID
+    let team: Team
+    var hasMoved: Bool
+    
+    init(team: Team){
+        self.id = UUID()
+        self.team = team
+        self.hasMoved = false
+    }
+    
+    func possibleMovements(from position: Position, board: Board) -> [Position] {
+        var moves: [Position] = []
+        
+        let directions = [
+            (2,1),
+            (2,-1),
+            (-2,1),
+            (-2,-1),
+            (-1,2),
+            (-1,-2),
+            (1,2),
+            (1,-2)
+            
+        ]
+        
+        for direction in directions {
+            if let targetPosition = position.offset(rowBy: direction.0, rankBy: direction.1){
+                if let targetPiece = board[position]{
+                    if targetPiece.team != team{
+                        moves.append(position)
+                    }
+                }
+                else{
+                    moves.append(position)
+                }
+            }
+        }
+        
+        
+        
+        
+        return moves
+        
+    }
+}
